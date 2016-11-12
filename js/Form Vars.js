2cbbda1b152902ebef4f6d941b7d4a1b389f0926
jlmoran <jacob.l.moran@gmail.com>
2016-11-12 20:41:10 +1100

    modified:   main.css 	modified:   welcome.html

---
+++ js/Form Vars.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+var $FormName= $('#name');
+	$FormName.append ('<value>"Hello"</value>')
+});
+
+/* // Write your jQuery code on line 3!
+$(document).ready(function() {
+var $target= $('#targ');
+    $target.fadeOut('fast');
+}); */
---
+++ js/FormVars.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+var $Invited= $('#Invitees');
+	$('#name').append("<p>$Invited</p>");
+});
+
+
+/* // Write your jQuery code on line 3!
+$(document).ready(function() {
+var $target= $('#targ');
+    $target.fadeOut('fast');
+}); */
