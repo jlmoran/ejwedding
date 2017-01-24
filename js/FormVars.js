b2d5c99dc260cd477d7356ff10a4a6db236348e4
jlmoran <jacobmoran@Jacobs-MacBook.local>
2017-01-24 14:41:57 +1100

    you love it

--- js/FormVars.js
+++ js/FormVars.js
@@ -1,4 +1,17 @@
-
+$(document).ready(function() {
 var Invite = getParameterByName('name');
+
+request = $.ajax({
+ url: "https://script.google.com/macros/s/AKfycbw8guOocngFA2qeneXw8WHD6LPwP59c5cb5INfNZLaXktfSruHD/exec", 
+        type: "get",
+        data: {name:Invite}
+    
+    })
+
+.done(function(invitereturn){
+    console.log (invitereturn)
+})
+    
    document.getElementById("Invitees").innerHTML = Invite;
    document.getElementById("name").value = Invite;
+})
