26d3c3ce22b096e784663c7a4cd5e4e2853a0dff
jlmoran <jacobmoran@Jacobs-MacBook.local>
2017-02-14 19:09:09 +1100

    Maybe?

--- js/FormVars.js
+++ js/FormVars.js
@@ -4,7 +4,7 @@ var Invite = getParameterByName('name');
 request = $.ajax({
  url: "https://script.google.com/macros/s/AKfycbzp0RZM4sPzjO-yymVA46wr-Ycj8aCiKF8c-z2GCWITJmROa40/exec", 
         type: "GET",
-        data: {cell:Invite}
+        data: {name:Invite}
     
     })
 
