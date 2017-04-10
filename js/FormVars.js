253c0647024e4d7a8d09e7f21ba7398115b85122
jlmoran <jacobmoran@Zool4.local>
2017-04-10 18:06:43 +1000

    Forms Var update for name
    Updated forms js to allow name from query string

--- js/FormVars.js
+++ js/FormVars.js
@@ -2,16 +2,17 @@ $(document).ready(function() {
 var Invite = getParameterByName('name');
 
 request = $.ajax({
- url: "https://script.google.com/macros/s/AKfycbzp0RZM4sPzjO-yymVA46wr-Ycj8aCiKF8c-z2GCWITJmROa40/exec", 
+url: "https://script.google.com/macros/s/AKfycbzp0RZM4sPzjO-yymVA46wr-Ycj8aCiKF8c-z2GCWITJmROa40/exec", 
         type: "GET",
-        data: {name:Invite}
+        data: {name:Invite},
+        dataType: "jsonp"
     
     })
 
 .done(function(invitereturn){
-    console.log (invitereturn)
+ document.getElementById("Invitees").innerHTML = invitereturn;
+   document.getElementById("name").value = invitereturn;
 })
     
-   document.getElementById("Invitees").innerHTML = Invite;
-   document.getElementById("name").value = Invite;
+  
 })
